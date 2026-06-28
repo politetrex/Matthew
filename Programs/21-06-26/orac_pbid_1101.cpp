f3896cc8cef992184a7c12e9248c78607bf6eb63
urbanetrex <urbanetrex@gmail.com>
2026-06-28 11:35:11 +0800

    twelvth commit

--- Programs/21-06-26/orac_pbid_1101.cpp
+++ Programs/21-06-26/orac_pbid_1101.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n,f; cin>>n>>f;
     vector<int> c(n); for(auto& i:c) cin>>i;
-    
-    return 0;
+    vector<long long> minp(n);
+    minp[0] = c[0];
+    for (int i=1; i<n; i++)
+        minp[i]=min(minp[i-1], (long long)c[i]);
+    int ans=-1;
+    for (int j=0; j<n; j++) {
+        long long maxc=f-c[j];
+        if (maxc<=0) continue;
+        int lo=0, hi=j; // bSearch.
+        while (lo<hi) {
+            int mid=(lo+hi)/2;
+            if (minp[mid]<=maxc)
+                hi=mid;
+            else lo=mid+1;
+        }
+        if (lo<j&&minp[lo]<=maxc)
+            ans=max(ans, j-lo+1);
+    }
+    cout<<ans; return 0;
 }
